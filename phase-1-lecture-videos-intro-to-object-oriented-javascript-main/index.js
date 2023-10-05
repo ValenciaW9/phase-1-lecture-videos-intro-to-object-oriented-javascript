dd04336e2ce15ec109e1599867cf1955ed6bd088
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-10-05 12:15:20 -0400

    phase-1-lecture-videos-intro-to-object-oriented-java
    phase-1-lecture-videos-intro-to-object-oriented-javascript/

---
+++ phase-1-lecture-videos-intro-to-object-oriented-javascript-main/index.js
@@ -0,0 +1,23 @@
+let garfield ={
+	name: "Garfield",
+	breed: "Persian Tabby",
+	introduce: function(){
+		return 'Mewow, my name is Garfield and I'm a Persian Tabby.'
+	}
+}
+
+let hellloKitty = {
+	name: "Hello Kitty",
+	breed: "Cartoom",
+	introduce: function(){
+		return 'Meow, my name is Hello Kitty and I'm a cartoom'.
+	}
+}
+
+let keyboardCat = {
+	name: "Keyboard Cat",
+	breed: "Yes",
+	introduce: function (){
+		return 'Meow, my name is Keyboard Cat and I like Bread.'
+	}
+}
